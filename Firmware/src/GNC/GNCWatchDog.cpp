18dc59bbb482c03e4ace79f07140e54afa497797
Arlo Maguire <maguirearlo@gmail.com>
2024-11-29 15:36:31 +0000

    Watchdog Timer added

--- Firmware/src/GNC/GNCWatchDog.cpp
+++ Firmware/src/GNC/GNCWatchDog.cpp
@@ -1,12 +1,38 @@
-// #include "GNCWatchDog.h"
-// void GNCWatchDog::pollPickle()
-// {
-//     SimpleCommandPacket test_command_1(3, 0);
-//     test_command_1.header.source_service = 10;
-//     test_command_1.header.source = 2;
-//     test_command_1.header.destination_service = 11;
-//     test_command_1.header.destination = 104;
-//     test_command_1.header.uid = 0;
-//     networkmanager.sendPacket(test_command_1);
-// }
-// }
+#include "GNCWatchDog.h"
+void GNCWatchDog::pollPickle()
+{
+    SimpleCommandPacket test_command_1(3, 0);
+    test_command_1.header.source_service = 10;
+    test_command_1.header.source = 2;
+    test_command_1.header.destination_service = 11;
+    test_command_1.header.destination = 104;
+    test_command_1.header.uid = 0;
+    networkmanager.sendPacket(test_command_1);
+
+}
+
+void GNCWatchDog::killMotors()
+{
+    //kill both the motors by setting them to 0
+    motor1.turnOff(); 
+    motor2.turnOff(); 
+}
+
+void GNCWatchDog::update()
+{
+    //poll the pickle to get time since last test wtf does this return cuz idk 😭
+    pollPickle();
+    //
+    lastUpdate = pollPickle.time;
+
+
+    if (millis() - lastUpdate > timeout)
+    {
+        // kill motors
+        killMotors();
+    }
+    //sleep this function so we dont flood the canbus with requests - if we want to add more stuff, we could make a seperate thread so other stuff still runs
+    Sleep(100);
+}
+
+
--- Firmware/src/GNC/GNCWatchDog.h
+++ Firmware/src/GNC/GNCWatchDog.h
@@ -1,33 +1,39 @@
-// #include <librnp/default_packets/simplecommandpacket.h>
+#include <librnp/default_packets/simplecommandpacket.h>
 
-// class GNCWatchDog 
-// {
-// public:
-//     GNCWatchDog(uint32_t timeout, 
-//                 RnpNetworkManager &networkmanager, 
-//                 NRCRemoteProp &motor1, 
-//                 NRCRemoteProp &motor2) : 
-//                 networkmanager(networkmanager),
-//                 motor1(motor1),
-//                 motor2(motor2)
-//     {
-//     }
+class GNCWatchDog 
+{
+public:
+    
+    GNCWatchDog(uint32_t timeout, 
+                RnpNetworkManager &networkmanager, 
+                NRCRemoteProp &motor1, 
+                NRCRemoteProp &motor2) : 
+                timeout(timeout),
+                lastUpdate(0),
+                networkmanager(networkmanager),
+                motor1(motor1),
+                motor2(motor2)
+    {
+    }
+    
+    void update();
 
-//     void update()
-//     {
-//         if (millis() - last_update > timeout)
-//         {
-//             // If the motors are not moving
-//         };
-//     };
+ /*   void update()
+    {
+        if (millis() - last_update > timeout)
+        {
+            // kill motors
+        };
+    };
+*/
+private:
+    uint32_t timeout; 
+    uint32_t lastUpdate;
+    RnpNetworkManager &networkmanager;
+    NRCRemoteProp &motor1;
+    NRCRemoteProp &motor2;
 
-// private:
-//     uint32_t timeout;
-//     uint32_t last_update;
-//     RnpNetworkManager &networkmanager;
-//     NRCRemoteProp &motor1;
-//     NRCRemoteProp &motor2;
+    void pollPickle();
+    void killMotors();
 
-//     void pollPickle();
-
-// };
+};
